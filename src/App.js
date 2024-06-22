7365900e3983d84d39ebc5ac35dd9f14ca1763c9
Ankit Kokane <ankitkokane90@gmail.com>
2024-06-22 13:48:19 +0530

    added base folder structure and react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './routes/Home.jsx';
+import About from './routes/About.jsx';
+import Contact from './routes/Contact.jsx';
 function App() {
   return (
     <div className="bg-yellow-600">
-      Hello
+      <BrowserRouter>
+      <Routes>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
