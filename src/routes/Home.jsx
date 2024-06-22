7922bfefa365e055128e9282ff610172c9097e83
rashikolhapure <rashikolhapure@gmail.com>
2024-06-22 14:47:29 +0530

    Home page hero section

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 export default function Home() {
-  return <div className="min-h-screen">
-    Home
-  </div>;
+  return (
+    <div className="min-h-screen px-20 pt-20">
+      <div className="w-[60vw] ml-auto">
+        <span className="text-3xl font-serif font-semibold">Your</span> <span className="text-7xl font-serif font-semibold">pets bestfriend awaits</span>
+      </div>
+      <div className="w-[60vw] ml-auto">Welcome to PawPals, where your pet's happiness and social life take center stage! At PawPals, we believe that every pet deserves a best friend and plenty of playtime. Our platform connects pet owners and their furry friends with local play dates, ensuring that your pets enjoy safe, fun, and engaging experiences.</div>
+    </div>
+  );
 }
